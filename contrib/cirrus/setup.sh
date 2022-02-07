af006afcfd7c6a39f49ef693fa307d1614c9189b
Chris Evich <cevich@redhat.com>
2022-02-07 12:53:57 -0500

    Cirrus: Add get_ci_vm.sh support
    This necessitated porting `setup.sh` and `runner.sh` over from the
    Netavark repo.  While I dislike duplicating the code, there are some
    subtle differences.  Otherwise, a large portion is boiler-plate which
    is needed to facilitate corresponding experiences in CI and get_ci_vm.sh
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

---
+++ contrib/cirrus/setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# This script configures the CI runtime environment.  It's intended
+# to be used by Cirrus-CI, not humans.
+
+set -e
+
+source $(dirname $0)/lib.sh
+
+# Only do this once
+if [[ -r "/etc/ci_environment" ]]; then
+    msg "It appears ${BASH_SOURCE[0]} already ran, exiting."
+    exit 0
+fi
+trap "complete_setup" EXIT
+
+msg "************************************************************"
+msg "Setting up runtime environment"
+msg "************************************************************"
+show_env_vars
+
+# TODO: Remove this when images updated w/ this package
+dnf -y install bind-utils
+
+req_env_vars NETAVARK_URL
+
+set -x  # show what's happening
+curl --fail --location -o /tmp/netavark.zip "$NETAVARK_URL"
+mkdir -p /usr/libexec/podman
+cd /usr/libexec/podman
+unzip /tmp/netavark.zip
+chmod a+x /usr/libexec/podman/netavark
