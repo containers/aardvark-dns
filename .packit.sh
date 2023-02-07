dfea9a8c01b0a02c89391b0209fa9afea7320356
Lokesh Mandvekar <lsm5@fedoraproject.org>
2023-02-07 20:58:18 +0530

    [CI:DOCS] Clarify packit related comments
    Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>

--- .packit.sh
+++ .packit.sh
@@ -5,22 +5,22 @@
 
 set -eo pipefail
 
-# Get Version from HEAD
+# Get Version from Cargo.toml in HEAD
 VERSION=$(grep '^version' Cargo.toml | cut -d\" -f2 | sed -e 's/-/~/')
 
-# Generate source tarball
+# Generate source tarball from HEAD
 git archive --prefix=aardvark-dns-$VERSION/ -o aardvark-dns-$VERSION.tar.gz HEAD
 
 # RPM Spec modifications
 
-# Fix Version
+# Use the Version from Cargo.toml in rpm spec
 sed -i "s/^Version:.*/Version: $VERSION/" aardvark-dns.spec
 
-# Fix Release
+# Use Packit's supplied variable in the Release field in rpm spec
 sed -i "s/^Release:.*/Release: $PACKIT_RPMSPEC_RELEASE%{?dist}/" aardvark-dns.spec
 
-# Fix Source0
+# Use above generated tarball as Source in rpm spec
 sed -i "s/^Source:.*.tar.gz/Source: aardvark-dns-$VERSION.tar.gz/" aardvark-dns.spec
 
-# Fix autosetup
+# Use the right build dir for autosetup stage in rpm spec
 sed -i "s/^%autosetup.*/%autosetup -Sgit -n %{name}-$VERSION/" aardvark-dns.spec
