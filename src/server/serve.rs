containers/aardvark-dns

commit 224756d98986355d7f7d71d79c8012f0c7e4c29d

server: use only one tokio runtime
The current code spawned a new thread per server and then spawned a full
tokio runtime in the new thread. The of course makes no sense at all and
doesn't scale well for many networks.

As such fix this by creating one runtime only and spawn all task based
on that which should mean much less overhead.

Signed-off-by: Paul Holzinger <pholzing@redhat.com>

src/server/serve.rs
@@ -2,6 +2,7 @@ use crate::backend::DNSBackend;
 use crate::config;
 use crate::config::constants::AARDVARK_PID_FILE;
 use crate::dns::coredns::CoreDns;
+use anyhow::Context;
 use arc_swap::ArcSwap;
 use log::{debug, error, info};
 use signal_hook::consts::signal::SIGHUP;
@@ -16,8 +17,7 @@ use std::net::Ipv4Addr;
 use std::net::Ipv6Addr;
 use std::sync::Arc;
 use std::sync::OnceLock;
-use std::thread;
-use std::thread::JoinHandle;
+use tokio::task::JoinHandle;
 
 use std::fs::File;
 use std::io::prelude::*;
@@ -30,7 +30,7 @@ type Config = (
     HashMap<String, Vec<Ipv6Addr>>,
 );
 type ThreadHandleMap<Ip> =
-    HashMap<(String, Ip), (flume::Sender<()>, JoinHandle<Result<(), std::io::Error>>)>;
+    HashMap<(String, Ip), (flume::Sender<()>, JoinHandle<Result<(), anyhow::Error>>)>;
 
 pub fn create_pid(config_path: &str) -> Result<(), std::io::Error> {
     // before serving write its pid to _config_path so other process can notify
@@ -57,7 +57,8 @@ pub fn create_pid(config_path: &str) -> Result<(), std::io::Error> {
     Ok(())
 }
 
-pub fn serve(
+#[tokio::main]
+pub async fn serve(
     config_path: &str,
     port: u32,
     filter_search_domain: &str,
@@ -92,15 +93,15 @@ pub fn serve(
             }
 
             // Gracefully stop all server threads first.
-            stop_threads(&mut handles_v4, None);
-            stop_threads(&mut handles_v6, None);
+            stop_threads(&mut handles_v4, None).await;
+            stop_threads(&mut handles_v6, None).await;
 
             process::exit(0);
         }
 
-        stop_and_start_threads(port, backend, listen_ip_v4, &mut handles_v4, no_proxy);
+        stop_and_start_threads(port, backend, listen_ip_v4, &mut handles_v4, no_proxy).await;
 
-        stop_and_start_threads(port, backend, listen_ip_v6, &mut handles_v6, no_proxy);
+        stop_and_start_threads(port, backend, listen_ip_v6, &mut handles_v6, no_proxy).await;
 
         // Block until we receive a SIGHUP.
         loop {
@@ -130,7 +131,7 @@ fn parse_configs(config_path: &str, filter_search_domain: &str) -> Result<Config
 ///
 /// Stop threads corresponding to listen IPs no longer in the configuration and start threads
 /// corresponding to listen IPs that were added.
-fn stop_and_start_threads<Ip>(
+async fn stop_and_start_threads<Ip>(
     port: u32,
     backend: &'static ArcSwap<DNSBackend>,
     listen_ips: HashMap<String, Vec<Ip>>,
@@ -154,7 +155,7 @@ fn stop_and_start_threads<Ip>(
         .filter(|k| !expected_threads.contains(k))
         .cloned()
         .collect();
-    stop_threads(thread_handles, Some(to_shut_down));
+    stop_threads(thread_handles, Some(to_shut_down)).await;
 
     // Then we start any new threads.
     let to_start: Vec<_> = expected_threads
@@ -165,23 +166,16 @@ fn stop_and_start_threads<Ip>(
     for (network_name, ip) in to_start {
         let (shutdown_tx, shutdown_rx) = flume::bounded(0);
         let network_name_ = network_name.clone();
-        let handle = thread::spawn(move || {
-            if let Err(e) = start_dns_server(
+        let handle = tokio::spawn(async move {
+            start_dns_server(
                 network_name_,
                 ip.into(),
                 backend,
                 port,
                 shutdown_rx,
                 no_proxy,
-            ) {
-                error!("Unable to start server: {}", e);
-                return Err(std::io::Error::new(
-                    std::io::ErrorKind::Other,
-                    format!("Error while invoking start_dns_server: {}", e),
-                ));
-            }
-
-            Ok(())
+            )
+            .await
         });
 
         thread_handles.insert((network_name, ip), (shutdown_tx, handle));
@@ -191,8 +185,10 @@ fn stop_and_start_threads<Ip>(
 /// # Stop DNS server threads
 ///
 /// If the `filter` parameter is `Some` only threads in the filter `Vec` will be stopped.
-fn stop_threads<Ip>(thread_handles: &mut ThreadHandleMap<Ip>, filter: Option<Vec<(String, Ip)>>)
-where
+async fn stop_threads<Ip>(
+    thread_handles: &mut ThreadHandleMap<Ip>,
+    filter: Option<Vec<(String, Ip)>>,
+) where
     Ip: Eq + Hash + Copy,
 {
     let mut handles = Vec::new();
@@ -206,13 +202,12 @@ where
     }
 
     for handle in handles {
-        if let Err(e) = handle.join() {
-            error!("Error from thread: {:?}", e);
+        if let Err(e) = handle.await {
+            error!("Error from dns server: {:?}", e);
         }
     }
 }
 
-#[tokio::main]
 async fn start_dns_server(
     name: String,
     addr: IpAddr,
@@ -220,18 +215,8 @@ async fn start_dns_server(
     port: u32,
     rx: flume::Receiver<()>,
     no_proxy: bool,
-) -> Result<(), std::io::Error> {
-    match CoreDns::new(addr, port, name, backend, rx, no_proxy).await {
-        Ok(mut server) => match server.run().await {
-            Ok(_) => Ok(()),
-            Err(e) => Err(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                format!("unable to start CoreDns server: {}", e),
-            )),
-        },
-        Err(e) => Err(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            format!("unable to create CoreDns server: {}", e),
-        )),
-    }
+) -> Result<(), anyhow::Error> {
+    let mut server =
+        CoreDns::new(addr, port, name, backend, rx, no_proxy).context("new dns server")?;
+    server.run().await.context("run dns server")
 }

src/dns/coredns.rs
@@ -43,8 +43,7 @@ pub struct CoreDns {
 impl CoreDns {
     // Most of the arg can be removed in design refactor.
     // so dont create a struct for this now.
-    #[allow(clippy::too_many_arguments)]
-    pub async fn new(
+    pub fn new(
         address: IpAddr,
         port: u32,
         network_name: String,

commit 48754fabdf6b0e1585c51eb1e003be2f26eca298

serve: fix broken error logging
Turns out the task contains a result in a result and we were only
looking at task error caused by tokio and not the actual result returned
from start_dns_server().
This caused all bind error to go nowhere and no even logged by in the
journal.

Fixes: 224756d ("server: use only one tokio runtime")

Signed-off-by: Paul Holzinger <pholzing@redhat.com>

src/server/serve.rs
@@ -216,8 +216,17 @@ async fn stop_threads<Ip>(
     }
 
     for handle in handles {
-        if let Err(e) = handle.await {
-            error!("Error from dns server: {:?}", e);
+        match handle.await {
+            Ok(res) => {
+                // result returned by the future, i.e. that actual
+                // result from start_dns_server()
+                if let Err(e) = res {
+                    // special anyhow error format to include cause but do not print backtrace
+                    error!("Error from dns server: {:#}", e)
+                }
+            }
+            // error from tokio itself
+            Err(e) => error!("Error from dns server task: {}", e),
         }
     }
 }